39cb4fe0d5954a3370566ed3fefb827234f474b9
yangrd <2445213759@qq.com>
2019-05-13 17:43:34 +0800

    Feature: pocket-run
    Signed-off-by: yangrd <2445213759@qq.com>

---
+++ ipress-run/src/main/java/com/yangrd/ipress/IPressRunApplication.java
@@ -0,0 +1,17 @@
+package com.yangrd.ipress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangrd
+ * @date 2019/05/13
+ */
+@SpringBootApplication
+public class IPressRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPressRunApplication.class, args);
+    }
+
+}
