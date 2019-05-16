65742032da7c4af174c3e7d881f520c6e0997e3c
yangrd <2445213759@qq.com>
2019-05-16 20:02:07 +0800

    Feature: 页面优化
    Signed-off-by: yangrd <2445213759@qq.com>

--- ipress-ui/src/views/Pocket.vue
+++ ipress-ui/src/views/Pocket.vue
@@ -49,6 +49,22 @@
             margin: 20px;
         }
     }
+    .layout{}
+    .author-ctl{
+        background: #f6f6f6;
+        width: 50px;
+        height: 100px;
+        position: fixed;
+        right: 50px;
+        top: 150px;
+        padding: 10px;
+        text-align: center;
+        border-radius: 16px;
+        border: 1px solid #f1f1f1;
+    }
+    .layout{
+        background: #fff;
+    }
 </style>
 <style>
     .ivu-layout-sider-trigger {
@@ -70,7 +86,7 @@
 <template>
     <div class="layout">
         <Layout :style="{minHeight: '100vh'}">
-            <Sider collapsible :collapsed-width="78" v-model="isCollapsed" :width="300" :style="{background: '#fff'}">
+            <Sider collapsible :collapsed-width="78" v-model="isCollapsed" :width="300" :style="{background: '#fff',}">
                 <div v-show="!isCollapsed">
                     <div style="margin: 20px;box-sizing: border-box">
                         <Input placeholder="输入关键字后按回车进行搜索"/>
@@ -82,7 +98,7 @@
                                 </Tooltip>
                             </div>
                             <div>
-                                <Tooltip content="新建目录" placement="left">
+                                <Tooltip content="新建/管理目录" placement="left">
                                     <Button shape="circle" type="primary" icon="ios-folder-outline"
                                             style="margin: 5px 8px" @click="openFolderDrawer"></Button>
                                 </Tooltip>
@@ -142,18 +158,19 @@
                     </Menu>
                 </div>
                 <div v-show="isCollapsed" style="text-align: center;padding: 5px">
-                    <h2 style="cursor: pointer" @click="$router.push({name:'home'})">IPress</h2>
+                    <h2 style="cursor: pointer; margin-top: 10px" @click="$router.push({name:'home'})">IPress</h2>
                     <div class="ctrl">
                         <ul>
                             <li>
-                                <Tooltip content="新建目录" placement="right">
-                                    <Button shape="circle" type="primary" icon="ios-folder-outline"
-                                            @click="openFolderDrawer"></Button>
-                                </Tooltip>
-                            </li>
-                            <li>
                                 <Tooltip content="新建页面" placement="right">
                                     <Button shape="circle" type="primary" icon="ios-add" @click="openPageDrawer"></Button>
+                                </Tooltip>
+
+                            </li>
+                            <li>
+                                <Tooltip content="新建/管理目录" placement="right">
+                                    <Button shape="circle" type="primary" icon="ios-folder-outline"
+                                            @click="openFolderDrawer"></Button>
                                 </Tooltip>
                             </li>
                         </ul>
@@ -178,34 +195,43 @@
 
             <Layout>
 
-                <Content :style="{padding: '0 16px 16px'}">
-
+                <Content :style="{paddingLeft: ' 0 '}">
                     <Card>
-                        <div style="height: 600px">Content</div>
+                        <div style="min-height: 1600px; width: 900px;margin: 0 auto">
+                            <h1>title</h1>
+                            <br>
+                            <div style=" border-top: 1px solid #e8e8e8;"></div>
+                            <br>
+                            Content
+                        </div>
                     </Card>
                 </Content>
             </Layout>
         </Layout>
 
-       <Drawer title="新建目录" placement="left" :closable="false" v-model="folderModel" width="33%">
-           <Form :model="formItem" :label-width="80">
-               <FormItem label="目录名">
-                   <Input v-model="formItem.input" placeholder="Enter something..."></Input>
-               </FormItem>
-               <FormItem label="上级目录">
-                   <Select v-model="formItem.select">
-                       <Option value="beijing">New York</Option>
-                       <Option value="shanghai">London</Option>
-                       <Option value="shenzhen">Sydney</Option>
-                   </Select>
-               </FormItem>
-           </Form>
-            <div class="demo-drawer-footer">
-                <Button style="margin-right: 8px" @click="folderModel = false">Cancel</Button>
-                <Button type="primary" @click="folderModel = false">Submit</Button>
-            </div>
+       <Drawer  :closable="false" v-model="folderModel" width="50%">
+           <Button shape="circle" @click="folderModel2=true">添加目录</Button>
+           <br><br>
+           <Table stripe :columns="columns1" :data="data1"></Table>
+
+
         </Drawer>
-        <Drawer title="新建页面" placement="right" :closable="false" v-model="pageModel" width="100%">
+        <Drawer  　:closable="false"　width="50%" v-model="folderModel2">
+            <br> <br>
+            <Form :model="formItem" :label-width="80">
+                <FormItem label="目录名">
+                    <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+                </FormItem>
+                <FormItem label="上级目录">
+                    <Select v-model="formItem.select">
+                        <Option value="beijing">New York</Option>
+                        <Option value="shanghai">London</Option>
+                        <Option value="shenzhen">Sydney</Option>
+                    </Select>
+                </FormItem>
+            </Form>
+        </Drawer>
+        <Drawer   :closable="false" v-model="pageModel" width="50%">
            <span style="margin-right: 10px">标题</span> <Input placeholder="标题"style="width: 200px"/>
             <span style="margin-right: 10px;margin-left: 20px">目录</span><Cascader :data="data" v-model="value1" style="width: 200px;display: inline-block"></Cascader>
             <br> <br>
@@ -213,10 +239,20 @@
 <!--            <mavon-editor v-model="value"/>-->
             <markdown-editor v-model="value"　:configs="configs" ref="markdownEditor"></markdown-editor>
            <div class="demo-drawer-footer">
-               <Button style="margin-right: 8px" @click="pageModel = false">Cancel</Button>
-               <Button type="primary" @click="pageModel = false">Submit</Button>
+               <Button style="margin-right: 8px" @click="pageModel = false">取消</Button>
+               <Button type="primary" @click="pageModel = false">保存</Button>
            </div>
        </Drawer>
+
+        <div class="author-ctl">
+            <Tooltip content="编辑" placement="right">
+            <Icon type="ios-create-outline" size="30" />
+            </Tooltip>
+            <br><br>
+            <Tooltip content="删除" placement="right">
+            <Icon type="ios-trash-outline"  size="30"/>
+            </Tooltip>
+        </div>
     </div>
 </template>
 <script>
@@ -227,6 +263,7 @@
                 activeMenuName: '1',
                 isCollapsed: false,
                 folderModel: false,
+                folderModel2: false,
                 pageModel: false,
                 value: '# hello \n' +
                     '> 你好',
@@ -256,6 +293,72 @@
                         }]
                     }]
                 }],
+                columns1: [
+                    {
+                        title: '目录',
+                        key: 'name'
+                    },
+                    {
+                        title: '添加时间',
+                        key: 'age'
+                    },
+                    {
+                        title: '序号',
+                        key: 'address'
+                    },
+                    {
+                        title: '操作',
+                        fixed: 'right',
+                        width: 120,
+                        render:(h,params)=>{
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'text',
+                                        size: 'small'
+                                    },
+                                    on:{
+                                        click:()=>{
+                                            this.folderModel2=true
+                                        }
+                                    }
+                                }, '编辑'),
+                                h('Button', {
+                                    props: {
+                                        type: 'text',
+                                        size: 'small'
+                                    }
+                                }, '删除')
+                            ]);
+                        }
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ],
                 formItem: {
                     input: '',
                     select: '',
