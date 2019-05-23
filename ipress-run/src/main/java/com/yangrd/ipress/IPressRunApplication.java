44c9b8bcecc3d10ed03600674f7beb11b933a3ae
yangrd <2445213759@qq.com>
2019-05-23 16:49:14 +0800

    feature: 增加https
    Signed-off-by: yangrd <2445213759@qq.com>

--- ipress-run/src/main/java/com/yangrd/ipress/IPressRunApplication.java
+++ ipress-run/src/main/java/com/yangrd/ipress/IPressRunApplication.java
@@ -1,8 +1,16 @@
 package com.yangrd.ipress;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author yangrd
@@ -11,6 +19,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class IPressRunApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080","https://sunxyz.gitee.io")
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IPressRunApplication.class, args);
---
+++ ipress-ui/src/api/axios.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+axios.defaults.baseURL='https://192.168.2.25:8086'
+export default axios
